a11755640072206506420528652b765f5eb2bb74
Sergey Ulyashev <sergey.ulyashev@gmail.com>
2023-11-29 13:35:50 +0300

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import 'react-toastify/ReactToastify.min.css';
 
 import { ProfilePage } from 'modules/user';
 import { LoginPage, RegisterPage } from 'modules/auth';
